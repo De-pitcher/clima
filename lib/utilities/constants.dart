cf75884f6b9f772f027578e66a2cb3d9814a85ba
Sparkz EN <emmanwa000@gmail.com>
2022-06-05 18:46:47 +0100

    first commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Noto Sans',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Noto Sans',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Noto Sans',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
